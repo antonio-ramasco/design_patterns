290421e212f2f315229982f12fb1313a4e279667
tonyvola <ramasco@hotmail.co.uk>
2024-04-14 16:18:34 +0100

    added observer pattern

--- tests/behavioral/observer_test.py
+++ tests/behavioral/observer_test.py
@@ -1,2 +1,21 @@
+from behavioral.observer import (
+    ConcreteElementA,
+    ConcreteElementB,
+    ConcreteVisitor1,
+    ConcreteVisitor2,
+    client_code,
+)
+
+
 def test_observer():
-    raise NotImplementedError()
+    components = [ConcreteElementA()]
+
+    visitor1 = ConcreteVisitor1()
+    assert client_code(components, visitor1) == ["ConcreteVisitor1.operation A"]
+
+    components.append(ConcreteElementB())
+    visitor2 = ConcreteVisitor2()
+    assert client_code(components, visitor2) == [
+        "ConcreteVisitor2.operation A",
+        "ConcreteVisitor2.operation B",
+    ]
